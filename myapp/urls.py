18a33b3ccbab58fc3265f9c1ad6b46f37297bc93
shirusms <sylviashimwangi27@gmail.com>
2025-03-05 13:43:04 +0300

    initial commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from myapp import views
 urlpatterns = [
     path('',views.user_list),
-    path('Add/',views.AddUser)
+    path('Add/',views.AddUser),
+    path('Edit/<id>',views.EditUser),
+    path('Delete/<id>',views.DeleteUser),
+    path('View/<id>',views.ViewUser),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -14,4 +14,24 @@ def AddUser(request):
         return redirect('/')
     mydict['form'] = form
     return render(request,'add.html',mydict)
+def EditUser(request,id=None):
+    one_rec=User.objects.get(pk=id)
+    form = UserForm(request.POST or None,request.FILES or None,instance=one_rec)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request,'edit.html',{'form':form})
+def DeleteUser(request,id=None):
+    one_rec=User.objects.get(pk=id)
+    if request.method=='POST':
+        one_rec.delete()
+        return redirect('/')
+    return render(request,'delete.html')
+def ViewUser(request,id=None):
+    mydict={}
+    one_rec=User.objects.get(pk=id)
+    mydict['user'] = one_rec
+    return render(request,'view.html',mydict)
+
+
 
